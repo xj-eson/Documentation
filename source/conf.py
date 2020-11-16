fc5935adc00e32681aeda79b7b6592fac144afaf
Du Kedi <654858548@qq.com>
2020-11-16 14:31:06 +0800

    hidden link

--- source/conf.py
+++ source/conf.py
@@ -67,8 +67,24 @@ html_static_path = ['_static']
 # html_logo = './_static/favicon.ico'
 
 
+html_theme_options = {
+    # 'canonical_url': '',
+    # 'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+    # 'analytics_anonymize_ip': False,
+    # 'logo_only': False,
+    # 'display_version': True,
+    # 'prev_next_buttons_location': 'bottom',
+    # 'style_external_links': True,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
+    ## Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': True
+}
+
 html_context = {
-"display_github": False, # Add 'Edit on Github' link instead of 'View page source'
-"last_updated": True,
-"commit": False,
+"display_github": False
 }
