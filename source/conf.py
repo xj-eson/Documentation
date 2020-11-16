5c63603c88b224e502d5fd93eb73921715ae9605
xj-eson <74519585+xj-eson@users.noreply.github.com>
2020-11-16 14:40:30 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -86,5 +86,7 @@ html_theme_options = {
 }
 
 html_context = {
-"display_github": False
+"display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+"last_updated": True,
+"commit": False
 }
